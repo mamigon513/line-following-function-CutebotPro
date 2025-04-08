b8276ee70652c24498ccf34259249241dbc7532f
mamigon513 <mamigon@nd.edu>
2025-04-08 13:52:15 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ basic.pause(50)
 
 def turn_right(left, right, err):
     left = left + (abs(err)/3000)*maxturnspeed
-    right = right + (abs(err)/-3000)*maxturnspeed
+    right = right - (abs(err)/3000)*maxturnspeed
     # Set the change
     CutebotPro.pwm_cruise_control(left, right)
     #delay 0.01 sec
     basic.pause(10)
 def turn_left(left, right, err):
-    left = left + (abs(err)/-3000)*maxturnspeed
+    left = left - (abs(err)/3000)*maxturnspeed
     right = right + (abs(err)/3000)*maxturnspeed
     # Set the change
     CutebotPro.pwm_cruise_control(left, right)
@@ -45,14 +45,30 @@ def follow_line():
     lwheel = 10
     rwheel = 10
     # get the line offset
-    error = CutebotPro.get_offset() +1000 # make the left side of line the center
+    error = CutebotPro.get_offset() + 500 # make the left side of line the center
     # if detects no line (both red)
-    if abs(error) > 2000:
+    if error >= 3000:
         lwheel = 0
         rwheel = 0
         #turn on both headlights (red)
         CutebotPro.color_light(CutebotProRGBLight.RGBL, 0xff0000)
         CutebotPro.color_light(CutebotProRGBLight.RGBR, 0xff0000)
+        basic.pause(10)
+    #### Intersection ####
+    # if detects a big line (error is less than 100)
+    #if abs(error) < 100:
+       # if error > 0: # robot is to the left of intersection (make a big right turn)
+       #     error = 3000/error
+        #    turn_right(lwheel, rwheel, error)
+      #      basic.pause(100)
+            #yellow light
+      #      CutebotPro.color_light(CutebotProRGBLight.RGBL, 0xffff00)
+      #  elif error < 0: # robot is to the right of intersection (make a big left turn)
+      #      error = 3000/error
+       #     turn_left(lwheel, rwheel, error)
+      #      basic.pause(100)
+            #yellow light
+      #      CutebotPro.color_light(CutebotProRGBLight.RGBR, 0xffff00)
     
     # too far left (left red)
     if error > 0:
--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ CutebotPro.pwmCruiseControl(10, 10)
 basic.pause(50)
 function turn_right(left: number, right: number, err: number) {
     left = left + Math.abs(err) / 3000 * maxturnspeed
-    right = right + Math.abs(err) / -3000 * maxturnspeed
+    right = right - Math.abs(err) / 3000 * maxturnspeed
     //  Set the change
     CutebotPro.pwmCruiseControl(left, right)
     // delay 0.01 sec
@@ -22,7 +22,7 @@ function turn_right(left: number, right: number, err: number) {
 }
 
 function turn_left(left: number, right: number, err: number) {
-    left = left + Math.abs(err) / -3000 * maxturnspeed
+    left = left - Math.abs(err) / 3000 * maxturnspeed
     right = right + Math.abs(err) / 3000 * maxturnspeed
     //  Set the change
     CutebotPro.pwmCruiseControl(left, right)
@@ -47,17 +47,33 @@ function follow_line() {
     let lwheel = 10
     let rwheel = 10
     //  get the line offset
-    let error = CutebotPro.getOffset() + 1000
+    let error = CutebotPro.getOffset() + 500
     //  make the left side of line the center
     //  if detects no line (both red)
-    if (Math.abs(error) > 2000) {
+    if (error >= 3000) {
         lwheel = 0
         rwheel = 0
         // turn on both headlights (red)
         CutebotPro.colorLight(CutebotProRGBLight.RGBL, 0xff0000)
         CutebotPro.colorLight(CutebotProRGBLight.RGBR, 0xff0000)
+        basic.pause(10)
     }
     
+    // ### Intersection ####
+    //  if detects a big line (error is less than 100)
+    // if abs(error) < 100:
+    //  if error > 0: # robot is to the left of intersection (make a big right turn)
+    //      error = 3000/error
+    //     turn_right(lwheel, rwheel, error)
+    //       basic.pause(100)
+    // yellow light
+    //       CutebotPro.color_light(CutebotProRGBLight.RGBL, 0xffff00)
+    //   elif error < 0: # robot is to the right of intersection (make a big left turn)
+    //       error = 3000/error
+    //      turn_left(lwheel, rwheel, error)
+    //       basic.pause(100)
+    // yellow light
+    //       CutebotPro.color_light(CutebotProRGBLight.RGBR, 0xffff00)
     //  too far left (left red)
     if (error > 0) {
         turn_right(lwheel, rwheel, error)
